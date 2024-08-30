133a60c7dc5f873685835389014a3053cd147b83
Mac Abrams <fishbock@gmail.com>
2024-08-30 13:47:17 -0400

    first half

--- inClass/Lecturer.h
+++ inClass/Lecturer.h
@@ -18,16 +18,22 @@ class Lecturer{
             office = "Innovation E220";
             class1 = class2 = class3 = class4 = 1000;
         }  
-        Lecturer(string name, string office){
+        Lecturer(string name, string office ,int class1 ,int class2 ,int class3 ,int class4){
             this->name = name;
             this->office = office;
-            this->name = name;
-            this->name = name;
-            this->name = name;
-            this->name = name;
+            this->class1 = class1;
+            this->class2 = class2;
+            this->class3 = class3;
+            this->class4 = class4;
             
         }
+        string getName() const{
+            return name;
+        }
 
+        void setName(){
+            this-> name = name;
+        }
 };
 
 #endif
